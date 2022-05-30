2cad5fc9324fda5176dbf26617abcbf7fdcb614d
Anderson <100314954+Anders1718@users.noreply.github.com>
2022-05-30 18:32:30 -0500

    Primer commit

---
+++ src/ventanas1/Conexion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanas1;
+
+/**
+ *
+ * @author User
+ */
+public class Conexion {
+    static String user = "sql5494677";
+    static String clave = "W1Gvnqdi9z";
+    static String url = "jdbc:mysql://sql5.freemysqlhosting.net/sql5494677";
+    
+    static String nombreNegocio = "Outlet";
+    
+    static String correo = "outletparatodos@outlook.com";
+    static String contrasena = "Outlet2022";
+}
